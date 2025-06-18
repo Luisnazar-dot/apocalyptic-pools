3f371badb293f4433af3efc88b921b38f9204798
Luis Alberto Perez Nazar <luis@example.com>
2025-06-18 15:46:47 -0600

    Agrega componente PredictionCard y actualiza main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './style.css';
+import PredictionCard from './components/PredictionCard';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PredictionCard />
   </React.StrictMode>
 );
+
