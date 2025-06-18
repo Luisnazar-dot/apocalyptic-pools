9d3985a2cd7654cc65c4a02858c2c155a9c770a8
Luis Alberto Perez Nazar <luis@example.com>
2025-06-18 17:17:46 -0600

    Renderiza PredictionCard en App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import PredictionCard from './components/PredictionCard';
 function App() {
   return (
     <div>
+      <h1 style={{ color: 'lime' }}>Apocalyptic Pools está VIVO ⚡</h1>
       <PredictionCard />
     </div>
   );
 }
 
 export default App;
+
 
